9bc2cc92d52f9e64c42fb50268f32f687ca1c1f5
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2021-10-04 15:26:33 -0700

    Set nav menu to open and close

--- starter/js/app.js
+++ starter/js/app.js
@@ -1,6 +1,10 @@
+// use to verify is connected. look at dev tools console
+console.log('hello world')
+
 const getElement = (selector) => {
   const element = document.querySelector(selector)
 
+  // if class is missing lets you know
   if (element) return element
   throw Error(
     `Please double check your class names, there is no ${selector} class`
@@ -10,6 +14,7 @@ const getElement = (selector) => {
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
 
+// listen for button click then do
 navBtnDOM.addEventListener('click', () => {
   links.classList.toggle('show-links')
 })
